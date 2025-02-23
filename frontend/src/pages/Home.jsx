adb37da803de00d1e9c4abf17ef9befeeb7d19f5
Dongni An <da2899@columbia.edu>
2025-02-23 09:11:09 +0800

    Use Hook to add state. Use form's input, value, onChange with state. Fix form's submit button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,26 @@
 import MovieCard from "../components/MovieCard"
+import {useState} from "react"
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         { id: 1, title: "The Lion King", release_date: 1999 },
         { id: 2, title: "The Matrix", release_date: 2000 },
         { id: 3, title: "John Wick", release_date: 1998 },
     ]
 
-    const handleSearch = () => {
-
+    const handleSearch = (e) => {
+        e.preventDefault() // don't allow the event onSumit refresh the page
+        alert(searchQuery) // no need to add curly bracket here, as this is a Javascript function, not JSX
+        setSearchQuery("--------------")
     }
 
     return <div className="home">
         <form onSubmit={handleSearch} className="search-form">
-            <input type="text" placeholder="Search for movies..." className="search-input" />
+            <input type="text" placeholder="Search for movies..." className="search-input" value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} />
+            <button type="submit" className="search-button">Search</button>
         </form>
-        <button type="submit" className="search-button">Search</button>
+        
 
         <div className="movies-grid">
             {
