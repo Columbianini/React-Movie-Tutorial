510a9a16144649b0119d8acd18da06c601c7ac49
Dongni An <da2899@columbia.edu>
2025-02-23 11:23:43 +0800

    Call API in JS

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,11 +1,13 @@
 import "../css/MovieCard.css"
+const MOVIE_MEDIA = "https://media.themoviedb.org/t/p/w500"
+
 function MovieCard({movie}) {
     function onFavoriteClick(){
         alert("clicked")
     }
     return <div className="movie-card">
         <div className="movie-poster">
-            <img src={movie.url} alt={movie.title} />
+            <img src={`${MOVIE_MEDIA}${movie.poster_path}`} alt={movie.title} />
             <div className="movie-overlay">
                 <button className="favorite-btn" onClick={onFavoriteClick}>❤️</button>
             </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,54 @@
 import MovieCard from "../components/MovieCard"
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import '../css/Home.css'
+import {searchMovies, getPopularMovies} from '../services/api'
+
 function Home() {
     const [searchQuery, setSearchQuery] = useState("");
 
-    const movies = [
-        { id: 1, title: "The Lion King", release_date: 1999 },
-        { id: 2, title: "The Matrix", release_date: 2000 },
-        { id: 3, title: "John Wick", release_date: 1998 },
-    ]
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const handleSearch = (e) => {
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } catch (err) {
+                console.log(err)
+                setError("Fail to load movies ...")
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+
+        loadPopularMovies()
+    }, []) // add side effect to components/functions and define when they should run depending on the list of dependencies
+    // const movies = [
+    //     { id: 1, title: "The Lion King", release_date: 1999 },
+    //     { id: 2, title: "The Matrix", release_date: 2000 },
+    //     { id: 3, title: "John Wick", release_date: 1998 },
+    // ]
+
+    const handleSearch = async (e) => {
         e.preventDefault() // don't allow the event onSumit refresh the page
-        alert(searchQuery) // no need to add curly bracket here, as this is a Javascript function, not JSX
-        setSearchQuery("--------------")
+        if (!searchQuery.trim()) {
+            return
+        }
+        if (loading) return
+        setLoading(true)
+        try {
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        } catch (err) {
+            console.log(err)
+            setError("Fail to search movies ...")
+        } finally {
+            setLoading(false)
+        }
     }
 
     return <div className="home">
@@ -21,14 +56,14 @@ function Home() {
             <input type="text" placeholder="Search for movies..." className="search-input" value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} />
             <button type="submit" className="search-button">Search</button>
         </form>
-        
-
-        <div className="movies-grid">
+        {error && <div className="error">{error}</div>}
+        {loading ? (<div className="loading">Loading...</div>) : 
+        (<div className="movies-grid">
             {
                 // for dynamic rendering, you need to have a unqiue "key" property for component even itself does not have the "key" property
-                movies.map(movie => (movie.title.toLowerCase().startsWith(searchQuery) && <MovieCard movie={movie} key={movie.id} />) )
+                movies.map(movie => <MovieCard movie={movie} key={movie.id} />)
             }
-        </div>
+        </div>)};
     </div>
 }
 
