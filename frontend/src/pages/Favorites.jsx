600f3887df899530dae2b60e44556eff7f2aac72
Dongni An <da2899@columbia.edu>
2025-02-23 09:39:21 +0800

    Add "Favorites" Page

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2>No favorite movie yet</h2>
+        <p>Start adding movies to your favorites and they wil appear here</p>
+    </div>
+}
+
+export default Favorites  
